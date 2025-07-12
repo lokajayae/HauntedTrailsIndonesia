83c129683fab812ae1ecbbca90dd8f9b4c49babe
Evan Lokajaya <48423337+lokajayae@users.noreply.github.com>
2025-07-12 21:40:36 +0700

    Remove footer from both pages

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,13 +1,11 @@
 import Header from "@/components/Header"
 import MapSection from "@/components/MapSection"
-import Footer from "@/components/Footer"
 
 export default function MapPage() {
   return (
     <div className="min-h-screen bg-black text-white">
       <Header />
       <MapSection />
-      <Footer />
     </div>
   )
 }
