de94a8edff52bb7778615f0c250f31653f0c3d31
Evan Lokajaya <48423337+lokajayae@users.noreply.github.com>
2025-07-12 22:42:25 +0700

    Fix Firestore configuration with environment variables

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,9 +3,9 @@ import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
   apiKey: process.env.NEXT_PUBLIC_GOOGLE_MAPS_API_KEY,
-  authDomain: `hauted-trails-db.firebaseapp.com`,
-  projectId: 'hauted-trails-db',
-  storageBucket: `hauted-trails-db.appspot.com`,
+  authDomain: `${process.env.NEXT_PUBLIC_FIRESTORE_DB}.firebaseapp.com`,
+  projectId: process.env.NEXT_PUBLIC_FIRESTORE_DB,
+  storageBucket: `${process.env.NEXT_PUBLIC_FIRESTORE_DB}.appspot.com`,
   messagingSenderId: '123456789',
   appId: '1:123456789:web:abcdef123456'
 }
